b3883851b7c7c7c882021977d8a7e3e9c1fb8429
Khasanova <nurgiza.hasanova@yandex.ru>
2026-05-22 18:35:43 +0500

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Lab_rab_2._0_KhasanovaNG_BPI_23_01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
